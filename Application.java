adb3286c3e42f85b56c9a02e0d1db1f30ce8c75d
fullstackltd <fullstackltd@gmail.com>
2023-11-23 20:16:25 +0600

    docs

--- Application.java
+++ Application.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Application {
 
   public static void main(String[] args) {
@@ -17,26 +19,69 @@ public class Application {
     // System.out.println("Hello World");
     // System.out.println("Hello World");
 
-  
     // Selection
     // if statement
-    int a = 10;
-    if (a == 9) {
-      System.out.println("True World");
-    }else{
-      System.out.println("False World");
-    }
+    // int a = 8;
+    // if (a == 9) {
+    // System.out.println("True World");
+    // }else if (a == 10) {
+    // System.out.println("False World");
+    // }else{
+    // System.out.println("Hello World");
+    // }
 
-    // if-else statement
     // switch statement
 
-    // Repetition
-    // while loop
-    // do-while loop
-    // for loop
-    // for-each loop
+    // Scanner input = new Scanner(System.in);
 
-    
+    // System.out.println("Menu");
+    // System.out.println("1. Choice 1");
+    // System.out.println("2. Choice 2");
+    // System.out.println("3. Choice 3");
+
+    // System.out.println("Enter a number: ");
+
+    // int choice = input.nextInt();
+    // switch (choice) {
+    // case 1:
+    // System.out.println("Choice 1");
+    // break;
+    // case 2:
+    // System.out.println("Choice 2");
+    // break;
+    // case 3:
+    // System.out.println("Choice 3");
+    // break;
+    // default:
+    // System.out.println("Default Choice");
+    // break;
+    // }
+
+    // Repetition / Iteration
+    // while loop
+
+    // int i = 0;
+    // while (i < 5) {
+    // System.out.println(i);
+    // i = i + 2;
+    // }
+
+    // do-while loop
+
+    // int j = 0;
+    // do {
+    // System.out.println("Hello World");
+    // j++;
+    // } while (j < 10);
+
+    // for loop
+    int k = 0;
+    for (; k < 5; ) {
+      System.out.println("Hello World" + k);
+      k++;
+    }
+
+    // for-each loop
 
     System.out.println("End of program");
   }
